8b98ebd5a9be7df93a08eda4fe4c44450cde664e
Mustafa Qureshi <mustafashakeel@gmail.com>
2021-11-23 11:10:48 -0800

    mongodb sort

---
+++ lesson1/sort.js
@@ -0,0 +1,19 @@
+var mongo = require('mongodb').MongoClient;
+
+var url = 'mongodb://localhost:27017/vanarts';
+
+mongo.connect(url, (err, db) => {
+    if (err) throw err;
+    var dbo = db.db("vanarts");
+    var query = { name: "Damel" };
+    var mysort = { name: 1 }
+    dbo.collection("students").find(query).sort(mysort).toArray((err, res) => {
+        if (err) throw err;
+        console.log("query ", res);
+        db.close();
+
+    });
+
+
+
+});
